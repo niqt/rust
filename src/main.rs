eeb32c4bcc46a1f814b4b6a6a64614f35eb085f5
Nicola De Filippo <nicola@nicoladefilippo.it>
2022-03-23 21:11:17 +0100

    ex57

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
-fn main() {
+fn f_to_c(f: f32) -> f32 {
+    return (f - 32.0) / 1.8
+}
+
+fn secret_number() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
@@ -27,4 +31,27 @@ fn main() {
             },
         }
     }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    let mut fib = 1;
+    if n == 1 || n == 2 {
+        return fib;
+    } else {
+        let mut nm1 = 1;
+        let mut nm2 = 1;
+
+        for _ in 2..n {
+            fib = nm1 + nm2;
+            nm2 = nm1;
+            nm1 = fib;
+        }
+    }
+    return fib
+}
+fn main() {
+    
+    println!("F° {} is C° {}:", 100, f_to_c(100.0));
+    println!("Fibonacci {}", fibonacci(8));
+    secret_number();
 }
